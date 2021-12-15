d455c04374dc10b7b155c8508b368e55c4acb5c7
NicolasPC13 <bytobihackpro4@gmail.com>
2021-12-14 18:11:26 -0800

    Segundo guardado, clases de variables primitivas

--- proyecto1/assets/js/alerts.js
+++ proyecto1/assets/js/alerts.js
@@ -1,5 +1,5 @@
 // alert('Hola mundo');
 // let nombre = promt('Quieres borrar esto?');
 
-const seleccion = confirm('Quieres borrar esto?');
-console.log(seleccion);
+//const seleccion = confirm('Quieres borrar esto?');
+//console.log(seleccion);
---
+++ proyecto1/assets/js/primitivos.js
@@ -0,0 +1,32 @@
+// declaracion de variable String, aqui lleva let ya que este es para inicializar
+// la variable
+let nombre = 'Peter Parker';
+console.log(nombre);
+
+//asignamos un nuevo valor a la variable
+nombre = 'Ben Parker';
+console.log(nombre);
+
+nombre = "Tia May";
+console.log(typeof nombre);
+
+nombre = 123;
+console.log(typeof nombre);
+
+let esMarvel = true;
+console.log( typeof esMarvel);
+
+let edad = 33;
+console.log( typeof edad);
+
+let superPoder;
+console.log( typeof superPoder);//????
+
+let soyNull = null;
+console.log( typeof soyNull);
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+console.log( typeof symbol1);
+
+console.log( symbol1 === symbol2 );
